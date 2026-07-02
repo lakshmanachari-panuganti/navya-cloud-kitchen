c5f86626ea403916dbd20959286bc9c0d4ea849e
lakshmanachari-panuganti <thesousi@gmail.com>
2026-07-02 10:39:27 +0530

    Rename palli_chikki image to palli_undalu
    Product is Bellam Palli Undalu (sweet peanut balls), not chikki.
    Renamed:
    - images/palli_chikki.png -> images/palli_undalu.png
    - source PNG in gemini brain folder
    - refs in app.js, copy-assets.js, copy_imgs.js
    
    Menu item id (bellam_palli_chikki) left unchanged - data-model
    identifier used as cart key, changing it would break existing carts.

--- copy_imgs.js
+++ copy_imgs.js
@@ -13,7 +13,7 @@ const filesToCopy = [
     { src: 'sunni_undalu_1780837017678.png', dest: 'sunni_undalu.png' },
     { src: 'kandi_podi_1780837030018.png', dest: 'kandi_podi.png' },
     { src: 'moringa_podi_1780837049362.png', dest: 'moringa_podi.png' },
-    { src: 'palli_chikki_1780837062371.png', dest: 'palli_chikki.png' },
+    { src: 'palli_undalu_1780837062371.png', dest: 'palli_undalu.png' },
     { src: 'navyas_kitchen_logo_1780835858832.png', dest: 'logo.png' }
 ];
 
